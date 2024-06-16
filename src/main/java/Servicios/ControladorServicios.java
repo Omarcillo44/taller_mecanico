ba5e63bf484af6ca12e5ee5dde1113eac2a70539
TeodoroRosalesMauricio <teodoro.rosales.mauriciio@gmail.com>
2024-06-16 12:07:48 -0600

    FXML Agendar Servicios

--- src/main/java/Servicios/ControladorServicios.java
+++ src/main/java/Servicios/ControladorServicios.java
@@ -1,4 +1,12 @@
 package Servicios;
 
-public class ControladorServicios {
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ControladorServicios{
+
 }
---
+++ src/main/java/Servicios/VistaServicios.java
@@ -0,0 +1,19 @@
+package Servicios;
+
+import com.example.taller_mecanico.HelloApplication;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.sql.SQLException;
+
+
+
+public class VistaServicios {
+
+}
